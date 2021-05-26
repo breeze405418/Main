8516a160d39dcd6384ed92647883d3dbdb0093cc
breeze405418 <breeze405418@gmail.com>
2021-05-26 22:57:31 +0800

    526

--- Web/JS/color.js
+++
@@ -1,9 +0,0 @@
-function CopyTextToClipboard(box1) {
-    var TextRange = document.createRange();
-    TextRange.selectNode(document.getElementById(box1));
-    sel = window.getSelection();
-        sel.removeAllRanges();
-        sel.addRange(TextRange);
-    document.execCommand("copy");
-    alert("複製完成！") //此行可加可不加
-  }
