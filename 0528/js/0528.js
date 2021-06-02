7b31dc1571f8f5164f07e73adb41a3366242c670
breeze405418 <breeze405418@gmail.com>
2021-06-02 18:39:13 +0800

    0602

---
+++ 0528/js/0528.js
@@ -0,0 +1,8 @@
+$(`#navbar-About`).click(function () {
+    $(`html,body`).animate({ scrollTop: $(`#Aboutus`).offset().top }, 1500);
+  });
+  $(`#2`).click(function () {
+    $(`html,body`).animate({ scrollTop: $(`#3`).offset().top }, 1500);
+  });
+  
+  
