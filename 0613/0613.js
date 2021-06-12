ff4cbf5c935976328561e65a6991dbac58516587
breeze405418 <breeze405418@gmail.com>
2021-06-13 01:19:02 +0800

    0613-0119

--- 0613/0613.js
+++ 0613/0613.js
@@ -5,4 +5,7 @@ const links = document.querySelectorAll('.nav-links li');
 
 hamburger.addEventListener('click',()=>{
     navlinks.classList.toggle("open");
+    links.forEach(link=>{
+        link.classList.toggle("fade")
+    });
 })
